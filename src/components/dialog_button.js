4da49e318f7e4db5385a1708455d5e50db1f78e3
Max Peiros <mpeiros@gmail.com>
2017-07-08 23:06:44 -0700

    Add charities to grid

--- src/components/dialog_button.js
+++ src/components/dialog_button.js
@@ -9,6 +9,12 @@ const customContentStyle = {
   height: '90%'
 };
 
+const styles = {
+  raisedButton: {
+    'margin-right': '10px'
+  }
+};
+
 class DialogButton extends Component {
   state = {
     open: false,
@@ -33,7 +39,7 @@ class DialogButton extends Component {
 
     return (
       <div>
-        <RaisedButton label="Learn More" onTouchTap={this.handleOpen} />
+        <RaisedButton label="Learn More" onTouchTap={this.handleOpen} style={styles.raisedButton} />
         <Dialog
           actions={actions}
           modal={true}
--- src/components/grid.js
+++ src/components/grid.js
@@ -12,29 +12,34 @@ const styles = {
   },
   gridList: {
     width: '95%',
-    overflowY: 'auto'
+    overflowY: 'auto',
+    margin: '20px'
   },
   image: {
-    width: '80%'
+    width: '50%'
   }
 };
 
 const tilesData = [
   {
+    img: 'http://www.sfsafehouse.org/uploads/9/5/0/0/95007226/1481844508.png',
+    title: 'San Francisco SafeHouse'
+  },
+  {
+    img: 'https://c394391.ssl.cf2.rackcdn.com/1393379892.jpg',
+    title: 'W.O.M.A.N. Inc.'
+  },
+  {
     img: 'https://upload.wikimedia.org/wikipedia/en/thumb/2/2c/Planned_Parenthood_logo.svg/1200px-Planned_Parenthood_logo.svg.png',
     title: 'Planned Parenthood'
   },
   {
-    img: 'https://pbs.twimg.com/profile_images/431950561949138944/ACdZkMDp.jpeg',
-    title: 'W.O.M.A.N. Inc.'
+    img: 'https://community.wwe.com/sites/default/files/komen_new_642.jpg',
+    title: 'Susan G. Komen'
   },
   {
-    img: 'http://www.sfsafehouse.org/uploads/9/5/0/0/95007226/1481843327.png',
-    title: 'San Francisco SafeHouse'
-  },
-  {
-    img: 'https://s3.amazonaws.com/givezooks_s3_production/media_files/199749/SF_DVC_logo.jpg',
-    title: 'San Francisco Domestic Violence Consortium'
+    img: 'http://womenscommunityclinic.org/wp-content/uploads/Oasis_WebHeaderLogo-620x265.png',
+    title: 'Oasis For Girls'
   },
   {
     img: 'http://www.kidslovecampme.com/wp-content/uploads/2013/01/header-e1395942660935.png',
@@ -51,7 +56,6 @@ class Grid extends Component {
           style={styles.gridList}
           padding={20}
         >
-          <Subheader>Charities</Subheader>
           {tilesData.map((tile) => (
             <GridTile
               key={tile.img}
