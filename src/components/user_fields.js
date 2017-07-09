1b87f8afb75dd2993637ba8f12e29e34998d5779
brn-gthbrg <eleanor@gmail.com>
2017-07-09 12:49:45 -0700

    dummy data for total money raised

--- src/components/user_fields.js
+++ src/components/user_fields.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 const styles = {
   field: {
@@ -7,6 +8,14 @@ const styles = {
   },
   age_field: {
     width: 60
+  },
+  money_box: {
+  	padding: 15,
+  	float: 'right'
+  },
+  prof_box: {
+  	padding: 15,
+  	float: 'left'
   },
   label: {
   	padding: 15
@@ -36,21 +45,30 @@ class TextFieldTest extends React.Component {
   render() {
     return (
       <div>
-        <label style={styles.label}><b>Name:</b></label><TextField
-          id="text-field-controlled"
-          value={this.state.name}
-          onChange={this.handleChange}
-        /><br/>
-        <label style={styles.label}><b>Age:</b></label><TextField style={styles.age_field}
-          id="text-field-controlled"
-          value={this.state.age}
-          onChange={this.handleChange}
-        /><br/>
-        <label style={styles.label}><b>Email:</b></label><TextField
-          id="text-field-controlled"
-          value={this.state.email}
-          onChange={this.handleChange}
-        />
+      	<div style={styles.label}>
+	      	<h2 style={styles.label}>Total Change collected: $14.64</h2>
+	      	<p style={styles.label}>Total for SF Safehouse: $8.23</p>
+	      	<p style={styles.label}>Total for W.O.M.A.N. Inc: $6.41</p>
+        </div><br/>
+        <div style={styles.label}>
+	        <h2 style={styles.label}>User Information</h2>
+	        <label style={styles.label}><b>Name:</b></label><TextField
+	          id="text-field-controlled"
+	          value={this.state.name}
+	          onChange={this.handleChange}
+	        /><br/>
+	        <label style={styles.label}><b>Age:</b></label><TextField style={styles.age_field}
+	          id="text-field-controlled"
+	          value={this.state.age}
+	          onChange={this.handleChange}
+	        /><br/>
+	        <label style={styles.label}><b>Email:</b></label><TextField
+	          id="text-field-controlled"
+	          value={this.state.email}
+	          onChange={this.handleChange}
+	        /><br/>
+	        <RaisedButton label="Save Profile" onTouchTap={this.handleOpen} style={styles.label} primary={true} />
+        </div>
       </div>
     );
   }
