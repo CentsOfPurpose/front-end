99b451cea60e7f97ea36029ae336695a9b0f6bfc
Max Peiros <mpeiros@gmail.com>
2017-07-08 19:43:24 -0700

    Add light blue 500 as primary palette color

--- src/containers/home_page.js
+++ src/containers/home_page.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import {lightBlue500} from 'material-ui/styles/colors';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import NavBar from '../components/nav_bar'
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: lightBlue500
+  }
+});
 
 class HomePage extends Component {
   render() {
     return (
-      <MuiThemeProvider>
+      <MuiThemeProvider muiTheme={muiTheme}>
         <div>
           <NavBar />
         </div>
