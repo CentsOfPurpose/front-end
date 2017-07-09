17d8220465f5c2ad6cd3672ee1f348abf6c4ae51
Max Peiros <mpeiros@gmail.com>
2017-07-08 17:42:45 -0700

    Add home page container

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import HomePage from './containers/home_page';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Switch>
+        <Route path="/" component={HomePage} />
+      </Switch>
+    </div>
+  </BrowserRouter>, 
+  document.getElementById('root')
+);
+
 registerServiceWorker();
